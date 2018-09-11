ac8bb7177561b72beded984779acf85db51a87af
kaluzsikc <kaluzsikc@gmail.com>
2018-09-11 13:41:42 +0100

    prepareDisplay working

--- lab_pubsub_planets_start/src/models/solar_system.js
+++ lab_pubsub_planets_start/src/models/solar_system.js
@@ -8,16 +8,19 @@ const SolarSystem = function() {
 
 SolarSystem.prototype.bindEvents = function () {
   PubSub.subscribe('PlanetList:select', (event) => {
-    const planetSelected = event.detail;
-    const result = planetSelected; //this.prepareDisplay();
+    const planetSelected = event.detail.id;
+    const result = this.prepareDisplay(planetSelected);
     console.log(result);
     PubSub.publish('Planets:planet-info', result);
   })
 };
-//
-// SolarSystem.prototype.prepareDisplay = function () {
-//   console.log(result);
-// };
+
+SolarSystem.prototype.prepareDisplay = function (info) {
+  const planet = this.planets.find(function(item){
+    return info === item.name
+  });
+  return planet;
+};
 
 
 module.exports = SolarSystem;
