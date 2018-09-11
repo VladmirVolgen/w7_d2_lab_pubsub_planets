4263d00a1d6dea0e903f23f1a168056282fc7951
kaluzsikc <kaluzsikc@gmail.com>
2018-09-11 12:10:28 +0100

    Add planet_list view

--- lab_pubsub_planets_start/src/app.js
+++ lab_pubsub_planets_start/src/app.js
@@ -1,7 +1,11 @@
 const planetsData = require('./data/planets.js');
 const SolarSystem = require('./models/solar_system.js');
+const PlanetList = require('./views/planet_list.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   const planetsDataModel = new SolarSystem(planetsData);
   console.log(planetsDataModel.planets);
+
+  const planetList = new PlanetList;
+  planetList.bindEvents();
 });
---
+++ lab_pubsub_planets_start/src/views/planet_list.js
@@ -0,0 +1,18 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const PlanetList = function(){
+  // this.element = element;
+};
+
+
+
+PlanetList.prototype.bindEvents = function () {
+  const planetSelected = document.querySelector('.planets-menu');
+  planetSelected.addEventListener('click', (event) => {
+    console.log(event.target.id);
+    const planet = document.querySelector(event.target.id);
+    PubSub.publish('PlanetList:select', planet)
+  })
+};
+
+module.exports = PlanetList;
