ae466361200f798fcc3138854fb79216e9470c4f
kaluzsikc <kaluzsikc@gmail.com>
2018-09-11 15:06:01 +0100

    Add css, rockets, yeah

--- lab_pubsub_planets_start/src/views/planet_display.js
+++ lab_pubsub_planets_start/src/views/planet_display.js
@@ -15,19 +15,22 @@ PlanetDisplay.prototype.displayResult = function (info) {
   const display = document.querySelector('.planet-details');
   display.innerHTML = this.render(info);
 };
-
+// REFACTOR to map each element and createElement
 PlanetDisplay.prototype.render = function (object) {
-  return `<ul>
-            <li>Name: ${object.name}</li>
-            <li>Orbit: ${object.orbit}</li>
-            <li>Day: ${object.day}</li>
-            <li>Surface Area: ${object.surfaceArea}</li>
-            <li>Volume: ${object.volume}</li>
-            <li>Gravity: ${object.gravity}</li>
-            <li>Moons: ${object.moons}</li>
-            <li><img src="${object.image}" width=10% height=10%></li>
-          </ul>
+  return `
+          <div class="planet-display" style="background-image: url('${object.image}');">
+            <ul>
+              <li>Name: ${object.name}</li>
+              <li>Orbit: ${object.orbit}</li>
+              <li>Day: ${object.day}</li>
+              <li>Surface Area: ${object.surfaceArea}</li>
+              <li>Volume: ${object.volume}</li>
+              <li>Gravity: ${object.gravity}</li>
+              <li>Moons: ${object.moons}</li>
+            </ul>
+          </div>
           `
+
 };
 
 module.exports = PlanetDisplay;
